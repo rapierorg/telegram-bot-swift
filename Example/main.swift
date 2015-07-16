1f74c86f70a3e5620932ccccc963d1afaef86ad3
Andrey Fidrya <af@zabiyaka.com>
2015-07-16 23:42:52 +0300

    Initial project structure

---
+++ Example/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Example
+//
+//  Created by Andrey Fidrya on 16/07/15.
+//  Copyright © 2015 Andrey Fidrya. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ Source/TeleBot.h
@@ -0,0 +1,19 @@
+//
+//  TeleBot.h
+//  TeleBot
+//
+//  Created by Andrey Fidrya on 16/07/15.
+//  Copyright © 2015 Andrey Fidrya. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TeleBot.
+FOUNDATION_EXPORT double TeleBotVersionNumber;
+
+//! Project version string for TeleBot.
+FOUNDATION_EXPORT const unsigned char TeleBotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Source/PublicHeader.h>
+
+
