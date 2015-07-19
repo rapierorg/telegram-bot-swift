437f50a65f78cd53eb6e333938ed0f82456c09cb
Andrey Fidrya <af@zabiyaka.com>
2015-07-19 13:23:31 +0300

    Add missing Optional PrettyPrint extension

--- Source/Categories/Optional+PrettyPrint.swift
+++ Source/Categories/Optional+PrettyPrint.swift
@@ -1,9 +1,45 @@
 //
-//  Optional+PrettyPrint.swift
-//  TeleBot
+// Optional+PrettyPrint.swift
 //
-//  Created by Andrey Fidrya on 19/07/15.
-//  Copyright © 2015 Andrey Fidrya. All rights reserved.
+// Copyright (c) 2015 Andrey Fidrya
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 import Foundation
+
+extension Optional {
+    /// Removes `Optional()` when printing optionals.
+    /// ```swift
+    /// var x: String? = "text"
+    /// var y: String?
+    /// print("\(x), \(y)")
+    /// print("\(x.prettyPrint), \(y.prettyPrint")
+    /// ```
+    /// Results in:
+    /// ```
+    /// Optional("text"), nil
+    /// text, nil
+    /// ```
+    var prettyPrint: String {
+        if let v = self {
+            return "\(v)"
+        }
+        return "nil"
+    }
+}
