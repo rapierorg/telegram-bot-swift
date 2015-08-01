cd45a5ada59e4198324a6fc2d40f83f985fb2d76
Andrey Fidrya <af@zabiyaka.com>
2015-08-01 03:13:37 +0300

    Add 'skip' family of functions to NSScanner

--- Source/Categories/NSScanner+Swift.swift
+++ Source/Categories/NSScanner+Swift.swift
@@ -15,9 +15,17 @@ extension NSScanner {
         return scanInt(&result) ? result : nil
     }
     
+    func skipInt32() -> Bool {
+        return scanInt(nil)
+    }
+    
     func scanInt() -> Int? {
         var result: Int = 0
         return scanInteger(&result) ? result : nil
+    }
+    
+    func skipInt() -> Bool {
+        return scanInteger(nil)
     }
     
     func scanInt64() -> Int64? {
@@ -25,9 +33,17 @@ extension NSScanner {
         return scanLongLong(&result) ? result : nil
     }
     
+    func skipInt64() -> Bool {
+        return scanLongLong(nil)
+    }
+    
     func scanUInt64() -> UInt64? {
         var result: UInt64 = 0
         return scanUnsignedLongLong(&result) ? result : nil
+    }
+    
+    func skipUInt64() -> Bool {
+        return scanUnsignedLongLong(nil)
     }
     
     func scanFloat() -> Float? {
@@ -35,9 +51,17 @@ extension NSScanner {
         return scanFloat(&result) ? result : nil
     }
     
+    func skipFloat() -> Bool {
+        return scanFloat(nil)
+    }
+    
     func scanDouble() -> Double? {
         var result: Double = 0.0
         return scanDouble(&result) ? result : nil
+    }
+    
+    func skipDouble() -> Bool {
+        return scanDouble(nil)
     }
     
     func scanHexUInt32() -> UInt32? {
@@ -45,9 +69,17 @@ extension NSScanner {
         return scanHexInt(&result) ? result : nil
     }
     
+    func skipHexUInt32() -> Bool {
+        return scanHexInt(nil)
+    }
+    
     func scanHexUInt64() -> UInt64? {
         var result: UInt64 = 0
         return scanHexLongLong(&result) ? result : nil
+    }
+    
+    func skipHexUInt64() -> Bool {
+        return scanHexLongLong(nil)
     }
     
     func scanHexFloat() -> Float? {
@@ -55,9 +87,17 @@ extension NSScanner {
         return scanHexFloat(&result) ? result : nil
     }
     
+    func skipHexFloat() -> Bool {
+        return scanHexFloat(nil)
+    }
+    
     func scanHexDouble() -> Double? {
         var result: Double = 0.0
         return scanHexDouble(&result) ? result : nil
+    }
+    
+    func skipHexDouble() -> Bool {
+        return scanHexDouble(nil)
     }
     
     func scanString(string: String) -> String? {
@@ -68,12 +108,20 @@ extension NSScanner {
         return nil
     }
     
+    func skipString(string: String) -> Bool {
+        return scanString(string, intoString: nil)
+    }
+    
     func scanCharactersFromSet(set: NSCharacterSet) -> String? {
         var result: NSString? = nil
         if scanCharactersFromSet(set, intoString: &result) {
             return result as? String
         }
         return nil
+    }
+    
+    func skipCharactersFromSet(set: NSCharacterSet) -> Bool {
+        return scanCharactersFromSet(set, intoString: nil)
     }
     
     func scanUpToString(string: String) -> String? {
@@ -84,11 +132,19 @@ extension NSScanner {
         return nil
     }
     
+    func skipUpToString(string: String) -> Bool {
+        return scanUpToString(string, intoString: nil)
+    }
+    
     func scanUpToCharactersFromSet(set: NSCharacterSet) -> String? {
         var result: NSString? = nil
         if scanUpToCharactersFromSet(set, intoString: &result) {
             return result as? String
         }
         return nil
+    }
+    
+    func skipUpToCharactersFromSet(set: NSCharacterSet) -> Bool {
+        return scanUpToCharactersFromSet(set, intoString: nil)
     }
 }
