50056547b04fd39a424b62b9d2ab8e26d26ffa2d
Andrey Fidrya <af@zabiyaka.com>
2016-05-23 14:55:44 +0300

    Use new API in hello-bot sample

--- Examples/hello-bot/Sources/main.swift
+++ Examples/hello-bot/Sources/main.swift
@@ -17,28 +17,31 @@ let bot = TelegramBot(token: token)
 
 let router = Router()
 
-router.addPath([Command("help")]) { () -> () in
+router.add(Command("help")) { () -> () in
     let helpText = "Usage: /greet"
     bot.respondPrivatelyAsync(helpText,
-        groupText: "\(bot.lastMessage.from.firstName), please find usage instructions in a personal message.")
+        groupText: "\(bot.lastMessage.from.first_name), please find usage instructions in a personal message.")
 }
 
-router.addPath([Command("greet")]) { () -> () in
-    bot.respondAsync("Hello, \(bot.lastMessage.from.firstName)!")
+router.add(Command("greet")) { () -> () in
+    bot.respondAsync("Hello, \(bot.lastMessage.from.first_name)!")
 }
 
 // Default handler
-router.addPath([RestOfString("text")]) { (args: Arguments) -> () in
-    let text = args["text"].stringValue
+router.fallbackHandler = .NonCancellableHandlerWithArguments({ (args: ArgumentScanner) -> () in
+    let text = args.scanRestOfString()
     bot.respondAsync("I don't understand \(text).\n" +
         "Usage: /greet")
-}
+})
 
 print("Ready to accept commands")
-while let command = bot.nextCommandSync() {
-    print("--- updateId: \(bot.lastUpdate!.updateId)")
-    print("message: \(bot.lastMessage.prettyPrint)")
-    try router.processString(command)
+while let message = bot.nextMessageSync() {
+	print("--- update_id: \(bot.lastUpdate!.update_id)")
+	print("message: \(message.debugDescription)")
+	print("command: \(bot.lastCommand.unwrapOptional)")
+
+	if let command = bot.lastCommand {
+		try router.process(command)
+	}
 }
 fatalError("Server stopped due to error: \(bot.lastError)")
-
