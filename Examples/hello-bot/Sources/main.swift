1d42884a587cabb8c77d9056f3be850e0a5676ac
Andrey Fidrya <af@zabiyaka.com>
2016-05-30 02:52:47 +0300

    Fix hello-bot example

--- Examples/hello-bot/Sources/main.swift
+++ Examples/hello-bot/Sources/main.swift
@@ -14,24 +14,22 @@ let bot = TelegramBot(token: token)
 
 let router = Router(bot: bot)
 
-router["help"] = { ()->() in
+router["help"] = { (context: Context) -> () in
     let helpText = "Usage: /greet"
-    bot.respondPrivatelyAsync(helpText,
-        groupText: "\(bot.lastMessage.from.first_name), please find usage instructions in a personal message.")
+    context.respondPrivatelyAsync(helpText,
+        groupText: "\(context.message.from.first_name), please find usage instructions in a personal message.")
 }
 
-router["greet"] = { ()->() in
-    bot.respondAsync("Hello, \(bot.lastMessage.from.first_name)!")
+router["greet"] = { (context: Context) -> () in
+    context.respondAsync("Hello, \(context.message.from.first_name)!")
 }
 
 print("Ready to accept commands")
 while let message = bot.nextMessageSync() {
-	print("--- update_id: \(bot.lastUpdate!.update_id)")
+	print("--- update_id: \(bot.lastUpdateId)")
 	print("message: \(message.debugDescription)")
-	print("command: \(bot.lastCommand.unwrapOptional)")
 
-	if let command = bot.lastCommand {
-		try router.process(command)
-	}
+	try router.process(message: message)
 }
+
 fatalError("Server stopped due to error: \(bot.lastError)")
