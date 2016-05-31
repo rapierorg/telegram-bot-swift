5206415199495005168a0ed12a29108e5edce25a
Andrey Fidrya <af@zabiyaka.com>
2016-05-31 04:22:26 +0300

    Allow using raw JSON result in requestAsync/Sync

---
+++ Sources/TelegramBot/Types/JSON+JsonObject.swift
@@ -0,0 +1,21 @@
+// Telegram Bot SDK for Swift (unofficial).
+// (c) 2015 - 2016 Andrey Fidrya. MIT license. See LICENSE for more information.
+
+
+import Foundation
+import SwiftyJSON
+
+extension JSON: JsonObject {
+	public init(json: JSON) {
+		self = json
+	}
+	
+	public var json: JSON {
+		get {
+			return self
+		}
+		set {
+			self = newValue
+		}
+	}
+}
