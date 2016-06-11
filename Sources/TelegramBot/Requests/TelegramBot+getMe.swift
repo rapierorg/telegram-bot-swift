315133f2894a1b093650956e2c1eaf349dc2a6f1
Andrey Fidrya <af@zabiyaka.com>
2016-06-12 02:46:17 +0300

    Fix getMeAsync visibility

--- Sources/TelegramBot/Requests/TelegramBot+getMe.swift
+++ Sources/TelegramBot/Requests/TelegramBot+getMe.swift
@@ -4,7 +4,7 @@
 import Foundation
 
 extension TelegramBot {
-	typealias GetMeCompletion = (result: User?, error: DataTaskError?)->()
+	public typealias GetMeCompletion = (result: User?, error: DataTaskError?)->()
 	
 	/// A simple method for testing your bot's auth token. Requires no parameters. Blocking version.
 	/// - Returns: Basic information about the bot in form of a `User` object.
@@ -18,7 +18,7 @@ extension TelegramBot {
     /// - Returns: Basic information about the bot in form of a `User` object.
     ///            Nil on error, in which case `error` contains the details.
 	/// - SeeAlso: <https://core.telegram.org/bots/api#getme>
-	private func getMeAsync(queue: dispatch_queue_t = dispatch_get_main_queue(),
+	public func getMeAsync(queue: dispatch_queue_t = dispatch_get_main_queue(),
 	                        completion: GetMeCompletion? = nil) {
 		requestAsync("getMe", completion: completion)
     }
