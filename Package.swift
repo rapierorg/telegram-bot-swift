b18ec42a74a4212c7737c5f4a308cf553d72ae11
Alireza <iAlirezaKML@icloud.com>
2017-04-21 18:49:40 +0430

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "telegram-bot-swift",
+    name: "TelegramBotSDK",
+    targets: [Target(name: "TelegramBotSDK")],
     dependencies: [
-	    .Package(url: "https://github.com/zmeyc/CCurl.git", majorVersion: 0),
+	.Package(url: "https://github.com/zmeyc/CCurl.git", majorVersion: 0),
         .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15),
         .Package(url: "https://github.com/smud/ScannerUtils.git", majorVersion: 1)
     ]
