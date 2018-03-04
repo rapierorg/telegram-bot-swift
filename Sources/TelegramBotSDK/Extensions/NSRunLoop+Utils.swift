7cb51fd4c435cc84df0cd3a40328f6e4e255fe9e
Andrey Fidrya <af@zabiyaka.com>
2018-03-04 04:32:54 +0300

    Remove Linux high CPU usage workaround (fixed in Swift 4)

--- Sources/TelegramBotSDK/Extensions/NSRunLoop+Utils.swift
+++ Sources/TelegramBotSDK/Extensions/NSRunLoop+Utils.swift
@@ -15,22 +15,8 @@ import Dispatch
 
 public extension RunLoop {
 	public func waitForSemaphore(_ sem: DispatchSemaphore) {
-        #if os(Linux)
-		while true {
-            // Bug workaround:
-            // RunLoop.run() on Linux waits in a busy loop producing high CPU usage. Also, it freezes if
-            // time interval is too small or negative. So, pick a minimal time interval when it still
-            // doesn't freeze and add usleep(). This will minimize CPU usage when idling.
-            let _ = run(mode: RunLoopMode.defaultRunLoopMode, before: Date(timeIntervalSinceNow: 0.002))
-		    if .success == sem.wait(timeout: DispatchTime.now()) {
-                break
-            }
-            usleep(100000)
-        }
-        #else
 		repeat {
             run(mode: RunLoopMode.defaultRunLoopMode, before: Date(timeIntervalSinceNow: 0.01))
 		} while .success != sem.wait(timeout: DispatchTime.now())
-        #endif
 	}
 }
