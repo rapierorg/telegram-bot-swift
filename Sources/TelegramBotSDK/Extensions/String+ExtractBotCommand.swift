7b3b78745e77fb5553f01fc1a36cf86a0a2f6dda
Andrey Fidrya <af@zabiyaka.com>
2018-03-04 04:07:15 +0300

    Revert more scanner changes

--- Sources/TelegramBotSDK/Extensions/String+ExtractBotCommand.swift
+++ Sources/TelegramBotSDK/Extensions/String+ExtractBotCommand.swift
@@ -21,21 +21,21 @@ extension String {
         scanner.charactersToBeSkipped = nil
         
         let whitespaceAndNewline = CharacterSet.whitespacesAndNewlines
-        scanner.scanCharacters(from: whitespaceAndNewline, into: nil)
-        
-        guard scanner.scanString("/", into: nil) else {
+        scanner.skipCharacters(from: whitespaceAndNewline)
+
+        guard scanner.skipString("/") else {
             return self
         }
         
         let alphanumericCharacters = CharacterSet.alphanumerics
-        guard scanner.scanCharacters(from: alphanumericCharacters, into: nil) else {
+        guard scanner.skipCharacters(from: alphanumericCharacters) else {
             return self
         }
 
         let usernameSeparatorIndex = scanner.scanLocation
 
         let usernameSeparator = "@"
-        guard scanner.scanString(usernameSeparator, into: nil) else {
+        guard scanner.skipString(usernameSeparator) else {
             return self
         }
 
@@ -44,9 +44,7 @@ extension String {
             "abcdefghijklmnopqrstuvwxyz" +
             "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
             "1234567890_")
-        var usernameNS: NSString?
-        guard scanner.scanCharacters(from: usernameCharacters, into: &usernameNS),
-                let username = usernameNS as String? else {
+        guard let username = scanner.scanCharacters(from: usernameCharacters) else {
             // Empty bot name. Treat as no bot name and process the comamnd.
             return self
         }
