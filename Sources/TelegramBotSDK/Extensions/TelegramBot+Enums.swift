828b9c04d3223bc3cb0fb32cdec65a27ae3aee58
Matteo Piccina <matteo.piccina.98@gmail.com>
2018-03-21 12:29:01 +0100

    Add enums file

---
+++ Sources/TelegramBotSDK/Extensions/TelegramBot+Enums.swift
@@ -0,0 +1,15 @@
+//
+//  TelegramBot+Enums.swift
+//  TelegramBotSDK
+//
+//  Created by Matteo Piccina on 21/03/18.
+//
+
+import Foundation
+
+extension TelegramBot {
+    public enum ParseMode: String {
+        case html = "HTML"
+        case markdown = "MarkDown"
+    }
+}
