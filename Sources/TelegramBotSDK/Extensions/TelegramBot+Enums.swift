ebb4bba78dddf54af33e6c0ce3e18383999f3753
Matteo Piccina <matteo.piccina.98@gmail.com>
2018-03-22 10:32:00 +0100

    Change headers

--- Sources/TelegramBotSDK/Extensions/TelegramBot+Enums.swift
+++ Sources/TelegramBotSDK/Extensions/TelegramBot+Enums.swift
@@ -1,10 +1,14 @@
 //
-//  TelegramBot+Enums.swift
-//  TelegramBotSDK
+// TelegramBot+Enums.swift
 //
-//  Created by Matteo Piccina on 21/03/18.
+// This source file is part of the Telegram Bot SDK for Swift (unofficial).
 //
-
+// Copyright (c) 2015 - 2016 Andrey Fidrya and the project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See LICENSE.txt for license information
+// See AUTHORS.txt for the list of the project authors
+//
 import Foundation
 
 extension TelegramBot {
