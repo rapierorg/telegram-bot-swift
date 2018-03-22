0f9ff74904bad81bff4ba5125dc4666dee1fa0c2
Matteo Piccina <matteo.piccina.98@gmail.com>
2018-03-22 10:32:00 +0100

    Change headers

--- Sources/TelegramBotSDK/Types/MessageEntity+Utils.swift
+++ Sources/TelegramBotSDK/Types/MessageEntity+Utils.swift
@@ -1,15 +1,20 @@
 //
-//  MessageEntity+Utils.swift
-//  TelegramBotSDKPackageDescription
+// MessageEntity+Utils.swift
 //
-//  Created by Matteo Piccina on 22/03/18.
+// This source file is part of the Telegram Bot SDK for Swift (unofficial).
+//
+// Copyright (c) 2015 - 2016 Andrey Fidrya and the project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See LICENSE.txt for license information
+// See AUTHORS.txt for the list of the project authors
 //
 
 import Foundation
 
 extension MessageEntity {
     // Type name is a reserved Swift word
-    public enum METype: String {
+    public enum MType: String {
         case mention
         case hashtag
         case botCommand = "bot_command"
