bf02cff20e33b8850d9878b72bab3c0d7a29bd75
Matteo Piccina <matteo.piccina.98@gmail.com>
2018-04-30 13:16:15 +0200

    Update hello-bot to new version

--- Examples/hello-bot/Package.swift
+++ Examples/hello-bot/Package.swift
@@ -1,9 +1,26 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "hello-bot",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .executable(
+            name: "hello-bot",
+            targets: ["hello-bot"]),
+        ],
     dependencies: [
-        .Package(url: "../..", majorVersion: 0)
-    ]
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "../..", .branch("dev")),
+        ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "hello-bot",
+            dependencies: ["TelegramBotSDK"]),
+        ]
 )
-
