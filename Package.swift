bbc35c5783870a72887d5d1420d7741d64655735
Andrey Fidrya <af@zabiyaka.com>
2018-05-20 22:48:03 +0300

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -24,8 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CCurl",
-            dependencies: [],
-            pkgConfig: "libcurl"),
+            dependencies: []),
         .target(
             name: "TelegramBotSDK",
             dependencies: ["CCurl"]),
