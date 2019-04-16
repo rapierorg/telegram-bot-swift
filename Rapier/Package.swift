aeeec58b388abbe5d90ec2d6ba4d9eed5dfd1414
Matteo Piccina <matteo.piccina@gmail.com>
2019-04-16 13:34:19 +0200

    Update YamlSwift dependency

--- Rapier/Package.swift
+++ Rapier/Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/behrang/YamlSwift.git", from: "3.4.3")
+        .package(url: "https://github.com/behrang/YamlSwift.git", from: "3.4.4")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
