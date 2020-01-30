3d964f8422b9da5beafe9a61342b2adecb6dc0e9
Matteo Piccina <matteo.piccina@gmail.com>
2020-01-30 16:16:46 +0100

    Extract MessageEntityType from Message type

--- Sources/TelegramBotSDK/Types/MessageEntity+Utils.swift
+++ Sources/TelegramBotSDK/Types/MessageEntity+Utils.swift
@@ -12,22 +12,20 @@
 
 import Foundation
 
-extension MessageEntity {
-    public enum MessageEntityType: String, Codable {
-        case mention
-        case hashtag
-        case cashtag
-        case botCommand = "bot_command"
-        case url
-        case email
-        case phoneNumber = "phone_number"
-        case bold
-        case italic
-        case underline
-        case strikethrough
-        case code
-        case pre
-        case textLink = "text_link"
-        case textMention = "text_mention"
-    }
+public enum MessageEntityType: String, Codable {
+    case mention
+    case hashtag
+    case cashtag
+    case botCommand = "bot_command"
+    case url
+    case email
+    case phoneNumber = "phone_number"
+    case bold
+    case italic
+    case underline
+    case strikethrough
+    case code
+    case pre
+    case textLink = "text_link"
+    case textMention = "text_mention"
 }
